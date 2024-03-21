5e68fe9a7bc12afde7df9de4c3f0d37d6529abdd
Angelo Clauin <contact@l31.dev>
2024-03-21 03:38:39 +0100

    fix: modified the cache logic
    I've put the fuse cache in the cache.ts file also I've added a error
    catching on the animes cache

--- api/src/cache.ts
+++ api/src/cache.ts
@@ -1,17 +1,34 @@
-import { NekoAnime } from "./types/anime.types"
+import Fuse from "fuse.js";
+import { NekoAnime } from "./types/anime.types";
 
-export let cachedAnimes: NekoAnime[] = []
+export let cachedAnimes: NekoAnime[] = [];
+let fuse: Fuse<NekoAnime> | null = null;
 
-async function getAllAnimes() {
-    const response = await fetch("https://neko.ketsuna.com/animes-search-vostfr.json")
-    const data = await response.json()
+export const getCachedFuse = (): Fuse<NekoAnime> => {
+    if (!fuse) {
+        fuse = new Fuse(cachedAnimes, {
+            includeScore: false,
+            keys: ["title", "title_english", "title_romanji", "title_french", "others"],
+        });
+    }
+    return fuse;
+};
 
-    cachedAnimes = data
+async function fetchAnimes(): Promise<NekoAnime[]> {
+    const response = await fetch("https://neko.ketsuna.com/animes-search-vostfr.json");
+    const data = await response.json();
+    return data;
 }
 
 export async function startCache() {
-    await getAllAnimes()
-    setInterval(getAllAnimes, 360000)
-    
-    console.info("cache refreshed")
+    try {
+        cachedAnimes = await fetchAnimes();
+        setInterval(async () => {
+            cachedAnimes = await fetchAnimes();
+            console.info("Cache refreshed");
+        }, 360000);
+        console.info("Cache initialized");
+    } catch (error) {
+        console.error("Failed to initialize cache:", error);
+    }
 }
--- api/src/routes/animes.routes.ts
+++ api/src/routes/animes.routes.ts
@@ -1,11 +1,9 @@
 import Elysia, { t } from "elysia"
-import { cachedAnimes } from "../cache"
 import Pino from "pino"
-import Fuse from "fuse.js"
+import { filterAnimes } from "../controllers/animes.controllers"
 
 export default function (app: Elysia<"/animes">) {
     const a = app.decorate("logger", Pino())
-    let fuse: Fuse<any>|null = null
 
     /* Retrieve a list of 25 animes using pagination */
     a.get(
@@ -14,29 +12,12 @@ export default function (app: Elysia<"/animes">) {
             const page = params.page || 1
             const { title, genres } = query
             
-            // TODO filter animes by title query
-            let tempAnimes = cachedAnimes
-            
-            if (title && title.trim() !== "") {
-                if (!fuse) fuse = new Fuse(cachedAnimes, {
-                    includeScore: false,
-                    keys: ["title", "title_english", "title_romanji", "title_french", "others"],
-                })
+            const animes = filterAnimes({
+                title,
+                genres: genres&&genres.trim()!==""?genres.split(","):undefined  
+            })
 
-                tempAnimes = fuse.search(title).map(a => a.item)
-            }
-
-            // TODO filter animes by genres (separated by commas)
-            if (genres && genres.trim() !== "") {
-                const splitedGenres = genres.split(",")
-                tempAnimes = tempAnimes.filter(anime => {
-                    return splitedGenres.every(genre => anime.genres.includes(genre));
-                })
-            }
-
-            // TODO filter animes by start year
-
-            return tempAnimes.slice(page * 25 - 25, page * 25)
+            return animes.slice(page * 25 - 25, page * 25)
         },
         {
             params: t.Object({
