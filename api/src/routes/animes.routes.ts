1b9edd16f2d5060c0d2bd3e91c6d72bd4ac18d5a
Angelo Clauin <contact@l31.dev>
2024-03-21 02:04:36 +0100

    feat: Add anime search by title functionality
    This commit introduces a new feature that allows users to search for
    animes by title. The API endpoint is as follows:
    
    `GET /animes/{id}`:
      - Parameters:
        - `id`: number (anime ID)
      - Query Parameters:
        - `title`: string (anime title to search for)
      - Response:
        - List of anime objects (maximum 25) with properties such as `id`, `title`, and `description`

--- api/src/routes/animes.routes.ts
+++ api/src/routes/animes.routes.ts
@@ -1,6 +1,7 @@
 import Elysia, { t } from "elysia"
 import { cachedAnimes } from "../cache"
 import Pino from "pino"
+import Fuse from "fuse.js"
 
 export default function (app: Elysia<"/animes">) {
     const a = app.decorate("logger", Pino())
@@ -8,20 +9,33 @@ export default function (app: Elysia<"/animes">) {
     /* Retrieve a list of 25 animes using pagination */
     a.get(
         "/:page",
-        ({ logger, params }) => {
+        ({ logger, params, query }) => {
             const page = params.page || 1
-            const animes = cachedAnimes.slice(page * 25 - 25, page * 25)
+            const { title } = query
+            
+            let tempAnimes = cachedAnimes
 
-            //TODO filter animes by title query
-            //TODO filter animes by genres (separated by commas)
-            //TODO filter animes by start year
+            const fuse = new Fuse(cachedAnimes, {
+                includeScore: false,
+                keys: ["title", "title_english", "title_romanji", "title_french", "others"],
+            })
+            
+            if (title && title.trim() !== "") {
+                tempAnimes = fuse.search(title).map(a => a.item)
+            }
 
-            logger.info(animes)
-            return animes
+            // TODO filter animes by genres (separated by commas)
+            // TODO filter animes by start year
+
+            return tempAnimes.slice(page * 25 - 25, page * 25)
         },
         {
             params: t.Object({
                 page: t.Numeric(),
+            }),
+
+            query: t.Object({
+                title: t.Optional(t.String()),
             }),
         },
     )
