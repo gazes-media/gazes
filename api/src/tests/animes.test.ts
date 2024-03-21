4f6cb9e1fdb99b44d870645d9479125212349100
Angelo Clauin <contact@l31.dev>
2024-03-21 03:52:54 +0100

    feat(api): add test for filtering animes by genres
    Add a new test case to `animes.test.ts` to ensure that the API
    endpoint correctly filters animes by specific genres. The test
    verifies that only animes with the specified genre are returned.

--- api/src/tests/animes.test.ts
+++ api/src/tests/animes.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "bun:test"
 import { app } from ".."
+import { Anime } from "../types/anime.types"
 
 describe("filterAnimes", () => {
     it("should have a 200 status", async () => {
@@ -10,5 +11,12 @@ describe("filterAnimes", () => {
     it("should return a array", async () => {
         const response = await app.handle(new Request("http://localhost/animes/1"))
         expect(await response.json()).toBeArray()
+    })
+
+    it("should return only animes with specific genres", async () => {
+        const response = await app.handle(new Request("http://localhost/animes/1?genres=action"))
+        const animes = await response.json() as Anime[]
+
+        expect(animes.every(anime => anime.genres.includes("action"))).toBe(true)
     })
 })
