18bdfd7cece4f74856b8e76b993116422e5d69c4
Angelo Clauin <contact@l31.dev>
2024-03-22 08:25:45 +0100

    feat(api): anime search endpoint
    Created a new endpoint ('/animes') that allow us to search animes by
    title, genres and release date.

--- api/src/controller/animeController.ts
+++ api/src/controller/animeController.ts
@@ -1,8 +1,24 @@
-import Elysia from 'elysia'
+import Elysia, { t } from 'elysia'
+import { filterAnimeList } from '../service/animeService'
+import { animeCache } from '../cache/animeCache'
 
 const router = new Elysia()
 export default router
 
-router.get('/animes', () => {
-  return 'hello world'
-})
+router.get(
+  '/animes',
+  ({ query }) => {
+    return filterAnimeList(animeCache, {
+      title: query.title,
+      releaseDate: query.releaseDate,
+      genreList: query.genreList?.split(','),
+    })
+  },
+  {
+    query: t.Object({
+      title: t.Optional(t.String()),
+      genreList: t.Optional(t.String()),
+      releaseDate: t.Optional(t.Numeric()),
+    }),
+  },
+)
--- api/src/service/animeService.ts
+++ api/src/service/animeService.ts
@@ -1,1 +1,24 @@
+import { animeCache } from '../cache/animeCache'
+import { getFuse } from '../cache/fuseCache'
+import type { NekosamaAnime } from '../type/animeType'
 
+export function filterAnimeList(
+  animeList: NekosamaAnime[],
+  { title, genreList, releaseDate }: { title?: string; genreList?: string[]; releaseDate?: number } = {},
+): NekosamaAnime[] {
+  if (genreList && genreList.length > 0 && genreList.every(genre => genre.trim() !== '')) {
+    animeList = animeList.filter(anime => genreList.every(genre => anime.genres.includes(genre)))
+  }
+
+  if (releaseDate) {
+    animeList = animeList.filter(anime => anime.start_date_year === releaseDate.toString())
+  }
+
+  if (title && title.trim() !== '') {
+    animeList = getFuse(animeList)
+      .search(title)
+      .map(anime => anime.item)
+  }
+
+  return animeList
+}
