5a3fa067c84059f8ca0d881bf63ba9faae037dc2
Angelo Clauin <contact@l31.dev>
2024-03-28 20:31:34 +0100

    WELLLLLL .... LETS USE NX

--- api/src/cache/animeCache.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'bun:test'
-import { animeCache, fillAnimeCache } from './animeCache'
-
-test('fillAnimeCache fetches data and updates animeCache', async () => {
-  await fillAnimeCache()
-  expect(animeCache.length).toBeGreaterThan(0)
-})
--- api/src/controller/index.ts
+++
@@ -1,7 +0,0 @@
-import Elysia from 'elysia'
-import animeController from './animeController'
-
-const router = new Elysia()
-export default router
-
-router.use(animeController)
