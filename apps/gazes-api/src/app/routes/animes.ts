23da02b8881358f015483f72e230a116cf045db7
Angelo Clauin <contact@l31.dev>
2024-03-29 23:16:30 +0100

    doc: document the /animes route

--- apps/gazes-api/src/app/routes/animes.ts
+++ apps/gazes-api/src/app/routes/animes.ts
@@ -12,14 +12,11 @@ import { AppOptions } from "main";
 
 export default async function (fastify: FastifyInstance, { redis, prisma }: AppOptions) {
   /**
-   * @fileoverview This handler provides a paginated list of animes, with
-   * optional filtering based on title, genres, status, and release year.
+   * @fileoverview This handler provides a paginated list of animes, with optional filtering based on title, genres, status, and release year.
    *
-   * The route defined by this handler is part of the Anime listing API,
-   * allowing clients to query for animes stored in the database with various
-   * filtering options to narrow down the search results. Pagination is
-   * implemented to limit the number of results returned in a single request,
-   * improving performance and usability for both the server and client.
+   * The route defined by this handler is part of the Anime listing API, allowing clients to query for animes stored in the database
+   * with various filtering options to narrow down the search results. Pagination is implemented to limit the number of results returned
+   * in a single request, improving performance and usability for both the server and client.
    *
    * Query Parameters:
    * - `page`: (Optional) The page number for pagination. Defaults to 1 if not specified.
@@ -27,30 +24,36 @@ export default async function (fastify: FastifyInstance, { redis, prisma }: AppO
    * - `genres`: (Optional) A comma-separated list of genres to filter animes by. An anime must match all specified genres.
    * - `status`: (Optional) The publication status of the anime to filter by (e.g., "ongoing", "completed").
    * - `releaseDate`: (Optional) The release year of the anime to filter by.
+   *
+   * Responses:
+   * - 200 OK: Successfully retrieved a list of animes based on the query parameters. The response body contains an array of anime objects.
+   * - 400 Bad Request: The request was malformed. This can happen if the query parameters are not in the expected format.
+   * - 500 Internal Server Error: An error occurred on the server while processing the request.
    */
-  fastify.get<{ Querystring: AnimeListQuerystring }>(
-    "/animes",
-    {
-      schema: { querystring: AnimeListQuerystringSchema },
-    },
-    async (req, rep) => {
+  fastify.get<{ Querystring: AnimeListQuerystring }>("/animes", {
+    schema: { querystring: AnimeListQuerystringSchema },
+  }, async (req, rep) => {
+     
+      // Extract query parameters with default values
       const { page = 1, title, genres, status, releaseDate } = req.query;
 
-      let findManyObject = {
-        skip: 25 * (page - 1),
-        take: 25 * page,
+      // Prepare the Prisma findMany query object with pagination and conditional filters
+      let queryOptions = {
+        skip: 25 * (page - 1), // Calculate offset for pagination
+        take: 25, // Limit the number of returned items to 25 for pagination
         where: {},
       };
 
-      if (title) findManyObject["where"]["others"] = { search: title.split(" ").join(" & ") };
-      if (genres) findManyObject["where"]["genres"] = { hasEvery: genres.split(",") };
-      if (status) findManyObject["where"]["status"] = { equals: status.toString() };
-      if (releaseDate) findManyObject["where"]["start_date_year"] = { equals: releaseDate.toString() };
+      // Conditional filters based on the query parameters provided by the client
+      if (title) queryOptions["where"]["others"] = { search: title.split(" ").join(" & ") };
+      if (genres) queryOptions["where"]["genres"] = { hasEvery: genres.split(",") };
+      if (status) queryOptions["where"]["status"] = { equals: status.toString() };
+      if (releaseDate) queryOptions["where"]["start_date_year"] = { equals: releaseDate.toString() };
 
-      console.log(findManyObject);
-
-      const receivedAnimeList = await prisma.anime.findMany(findManyObject);
-      rep.send(receivedAnimeList).status(200);
+      // Execute the query using the Prisma client and send the result back to the client
+      const animeList = await prisma.anime.findMany(queryOptions);
+      rep.status(200).send(animeList);
+      
     }
   );
 
