f9e0f234f7373da1e5297d8b6cbc9bf474f3cda0
Angelo Clauin <contact@l31.dev>
2024-03-29 23:26:07 +0100

    doc: document the /animes/:id route

--- apps/gazes-api/src/app/routes/animes.ts
+++ apps/gazes-api/src/app/routes/animes.ts
@@ -34,45 +34,64 @@ export default async function (fastify: FastifyInstance, { redis, prisma }: AppO
     schema: { querystring: AnimeListQuerystringSchema },
   }, async (req, rep) => {
      
-      // Extract query parameters with default values
-      const { page = 1, title, genres, status, releaseDate } = req.query;
+    // Extract query parameters with default values
+    const { page = 1, title, genres, status, releaseDate } = req.query;
 
-      // Prepare the Prisma findMany query object with pagination and conditional filters
-      let queryOptions = {
-        skip: 25 * (page - 1), // Calculate offset for pagination
-        take: 25, // Limit the number of returned items to 25 for pagination
-        where: {},
-      };
+    // Prepare the Prisma findMany query object with pagination and conditional filters
+    let queryOptions = {
+      skip: 25 * (page - 1), // Calculate offset for pagination
+      take: 25, // Limit the number of returned items to 25 for pagination
+      where: {},
+    };
 
-      // Conditional filters based on the query parameters provided by the client
-      if (title) queryOptions["where"]["others"] = { search: title.split(" ").join(" & ") };
-      if (genres) queryOptions["where"]["genres"] = { hasEvery: genres.split(",") };
-      if (status) queryOptions["where"]["status"] = { equals: status.toString() };
-      if (releaseDate) queryOptions["where"]["start_date_year"] = { equals: releaseDate.toString() };
+    // Conditional filters based on the query parameters provided by the client
+    if (title) queryOptions["where"]["others"] = { search: title.split(" ").join(" & ") };
+    if (genres) queryOptions["where"]["genres"] = { hasEvery: genres.split(",") };
+    if (status) queryOptions["where"]["status"] = { equals: status.toString() };
+    if (releaseDate) queryOptions["where"]["start_date_year"] = { equals: releaseDate.toString() };
 
-      // Execute the query using the Prisma client and send the result back to the client
-      const animeList = await prisma.anime.findMany(queryOptions);
-      rep.status(200).send(animeList);
-      
+    // Execute the query using the Prisma client and send the result back to the client
+    const animeList = await prisma.anime.findMany(queryOptions);
+    rep.status(200).send(animeList);
+
+  });
+
+  /**
+   * @fileoverview This handler retrieves detailed information for a specified anime identified by its unique ID.
+   * 
+   * The route defined by this handler is part of the Anime details API, enabling client to query detailed information
+   * about a single anime. This includes its title, synopsis, cover image, episode, and more. The anime ID used to fetch
+   * the details is provided as a URL parameter; This handler makes use of a service function 'getAnimeById' which abstracts
+   * the logic for retrieving the anime details from the database or cache, providing a clean separation of concerns.
+   * 
+   * URL Parameters:
+   * - 'id': The unique identifier of the anime to retrieve. This must be a valid integer corresponding to the ID of the anime
+   * in the database.
+   * 
+   * Responses:
+   * - 200 OK: Successfully retrieved the details of the requested anime. The response body contains the anime object.
+   * - 404 Not Found: No anime could be found for the given ID. This indicates either an invalid or that the requested anime 
+   * does not exists in the database
+   * - 500 Internal Server Error: An error occurered on the server while processing the request. This indicates an unexpected
+   * issue that prevented the server from fulfilling the request.
+   */
+  fastify.get<{ Params: AnimeDetailParams }>("/animes/:id", { 
+    schema: { params: AnimeDetailParamsSchema } 
+  }, async (req, rep) => {
+     
+    // Attempt to retrieve the anime details using the provided ID
+    const anime = await getAnimeById(prisma, redis, req.params.id);
+
+    // Check if the anime was found and respond accordingly
+    if (!anime) {
+      rep.status(404).send("Anime Not Found");
+      return;
     }
-  );
 
-  fastify.get<{ Params: AnimeDetailParams }>(
-    "/animes/:id",
-    { schema: { params: AnimeDetailParamsSchema } },
-    async (req, rep) => {
-      const { id } = req.params;
-
-      const receivedAnime = await getAnimeById(prisma, redis, id);
-
-      if (!receivedAnime) {
-        rep.status(404).send("Not Found");
-        return;
-      }
-
-      rep.status(200).send(receivedAnime);
-    }
-  );
+    // Respond with the found anime details
+    rep.status(200).send(anime);
+  
+  });
 
   fastify.get<{ Params: EpisodeParams }>(
     "/animes/:id/:ep",
