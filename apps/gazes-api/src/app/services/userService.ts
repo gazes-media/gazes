7ca1e7f846b11aad7df4647a682463f22319cf1a
Angelo Clauin <contact@l31.dev>
2024-03-30 05:30:57 +0100

    feat(services): add UserService for user registration and authentication operations

--- apps/gazes-api/src/app/services/userService.ts
+++ apps/gazes-api/src/app/services/userService.ts
@@ -3,13 +3,30 @@ import { hash, verify } from 'argon2';
 import { signJWT } from '../utils/jwtUtils';
 import { config } from '@api/config';
 
+/**
+ * Service class for handling user-related operations such as registration and authentication.
+ */
 export class UserService {
     private readonly prisma: PrismaClient;
 
+    /**
+     * Creates a UserService instance.
+     *
+     * @param {PrismaClient} prisma - The Prisma client instance to be used for database operations.
+     */
     constructor(prisma: PrismaClient) {
         this.prisma = prisma;
     }
 
+    /**
+     * Registers a new user with the given email, username, and password.
+     *
+     * @param {string} email - The email of the new user.
+     * @param {string} username - The username of the new user.
+     * @param {string} password - The password for the new user.
+     * @returns {Promise<{ user: { id: number; username: string; }, token: string }>}
+     *          An object containing the new user (excluding the password) and a JWT token.
+     */
     async registerUser(email: string, username: string, password: string) {
         const hashedPassword = await hash(password);
         const newUser = await this.prisma.user.create({
@@ -21,6 +38,15 @@ export class UserService {
         return { user: newUser, token };
     }
 
+    /**
+     * Authenticates a user based on email and password.
+     *
+     * @param {string} email - The email of the user trying to log in.
+     * @param {string} password - The password of the user trying to log in.
+     * @returns {Promise<{ user: { id: number; username: string; }, token: string }>}
+     *          An object containing the authenticated user (excluding the password) and a JWT token.
+     * @throws {Error} If the credentials are invalid or the user cannot be found.
+     */
     async authenticateUser(email: string, password: string) {
         const userWithPassword = await this.prisma.user.findUnique({
             where: { email },
