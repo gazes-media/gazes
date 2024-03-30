7b8c5f60d84e188da4c109d599f64f0d177f6779
Angelo Clauin <contact@l31.dev>
2024-03-30 05:31:34 +0100

    feat(routes): Adding JSDoc to auth routes

--- apps/gazes-api/src/app/routes/auth.ts
+++ apps/gazes-api/src/app/routes/auth.ts
@@ -1,69 +1,53 @@
 import { FastifyInstance } from 'fastify';
 import { AppOptions } from '@api/main';
-import argon2 from 'argon2';
 import { RegisterBody, LoginBody, LoginBodySchema, RegisterBodySchema } from '@api/contracts/authContract';
-import { signJWT } from '../utils/jwtUtils';
-import { config } from '@api/config';
 import { UserService } from '../services/userService';
 
+/**
+ * Initializes authentication routes.
+ *
+ * @param {FastifyInstance} fastify - The Fastify instance.
+ * @param {AppOptions} options - The application options including Redis and Prisma clients.
+ */
 export default async function (fastify: FastifyInstance, { redis, prisma }: AppOptions) {
     const userService = new UserService(prisma);
 
     /**
-     * @fileoverview This handler is responsible for registering the user.
+     * Registers a new user.
      *
-     * @description The handler takes the user's email, password, and username as input and creates a new user.
-     * It then returns the user object and sets a JWT token in the response header.
-     *
-     * Body Parameters:
-     * - email: string
-     * - password: string
-     * - username: string
-     *
-     * Response:
-     * - 201: User created successfully
-     * - 400: User already exists
-     *
+     * @route POST /register
+     * @param {RegisterBody} req.body - The user's email, password, and username.
+     * @returns The user object and a JWT token in the response header upon success.
+     * @response {201} User created successfully.
+     * @response {400} User already exists.
      */
-    fastify.post<{ Body: RegisterBody }>(
-        '/register',
-        {
-            schema: { body: RegisterBodySchema },
-        },
-        async function (req, rep) {
-            try {
-                const { email, username, password } = req.body;
-                const { user, token } = await userService.registerUser(email, username, password);
+    fastify.post<{ Body: RegisterBody }>('/register', { schema: { body: RegisterBodySchema } }, async (req, rep) => {
+        try {
+            const { email, username, password } = req.body;
+            const { user, token } = await userService.registerUser(email, username, password);
 
-                rep.header('Set-Cookie', `token=${token}; HttpOnly; Path=/; Secure; SameSite=Strict`);
-                rep.status(201).send(user);
-            } catch (e) {
-                if (e.code === 'P2002') {
-                    rep.status(400).send('User already exists');
-                    return;
-                }
-                rep.status(500).send('An unexpected error occurred');
+            rep.header('Set-Cookie', `token=${token}; HttpOnly; Path=/; Secure; SameSite=Strict`);
+            rep.status(201).send(user);
+        } catch (e) {
+            if (e.code === 'P2002') {
+                rep.status(400).send('User already exists');
+                return;
             }
+            rep.status(500).send('An unexpected error occurred');
         }
-    );
+    });
+
     /**
-     * @fileoverview This handler is responsible for authenticating the user.
+     * Authenticates a user.
      *
-     * @description The handler takes the user's email and password as input and verifies the credentials.
-     * If the credentials are valid, it returns the user object and sets a JWT token in the response header.
-     * Otherwise, it returns an error message.
-     *
-     * Body Parameters:
-     * - email: string
-     * - password: string
-     *
-     * Response:
-     * - 200: User authenticated successfully
-     * - 401: Invalid Credentials
-     * - 400: Invalid Credentials
-     *
+     * @route POST /login
+     * @param {LoginBody} req.body - The user's email and password.
+     * @returns The user object and a JWT token in the response header upon successful authentication.
+     * @response {200} User authenticated successfully.
+     * @response {401} Invalid Credentials.
+     * @response {400} Bad request, possibly due to malformed request syntax.
      */
-    fastify.post<{ Body: LoginBody }>('/login', { schema: { body: LoginBodySchema } }, async function (req, rep) {
+    fastify.post<{ Body: LoginBody }>('/login', { schema: { body: LoginBodySchema } }, async (req, rep) => {
         try {
             const { email, password } = req.body;
             const { user, token } = await userService.authenticateUser(email, password);
