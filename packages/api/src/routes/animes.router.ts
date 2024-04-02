e4f03a8c90d46f806ffc02c9aafe1f307c9f9ab3
Angelo Clauin <contact@l31.dev>
2024-04-02 03:59:16 +0200

    🚀 clean script & enhance types integration
    - Set project to private in root `package.json` to avoid accidental
      publishing.
    - Introduced a `clean` script in root `package.json` for easier
      workspace cleanup.
    - Updated `fastify` dependency and added `@gazes/types` as a workspace
      dependency in `packages/api/package.json` to ensure consistency and
      leverage shared types.
    - Refined imports and simplified the structure in
      `packages/api/src/routes/animes.router.ts` by utilizing shared types
      from `@gazes/types`, demonstrating a move towards more modular and
      maintainable code.
    - Initialized `packages/types` with essential configurations and a basic
      README for better clarity and documentation.
    - Enhanced TypeScript configuration across packages to better support
      modular architecture and shared types, ensuring smoother development
      and integration across workspaces.
    
    Binary files `bun.lockb` updated to reflect new dependency resolutions
    and package updates.

--- packages/api/src/routes/animes.router.ts
+++ packages/api/src/routes/animes.router.ts
@@ -1,10 +1,7 @@
-import type { RouteOptions } from "@/app"
-import type { FastifyInstance } from "fastify"
+import type { RouteOptions } from "@/app";
+import type { FastifyInstance } from "fastify";
+import type { IReply } from "@gazes/types";
 
-export default async function(app: FastifyInstance, opts: RouteOptions) {
-    app.get('/animes', (request, reply) => {
-        reply.status(200).send({
-            data: [{}]
-        })
-    })
+export default async function (app: FastifyInstance, opts: RouteOptions) {
+	app.get<{ Reply: IReply }>("/animes", (request, reply) => {});
 }
