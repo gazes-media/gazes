4f47a49beeab5748153abfb962cc1b44b7f6fe29
Angelo Clauin <contact@l31.dev>
2024-04-03 13:09:01 +0200

    🐛 fix(api): correct import path for RouteOptions in animeRouter
    - Correct the import path for `RouteOptions` to `@/types/routeTypes`, fixing a previously overlooked update.
    - Adjust the structure within `animeRouter.ts` for better readability.

--- packages/api/src/routers/animeRouter.ts
+++ packages/api/src/routers/animeRouter.ts
@@ -1,7 +1,9 @@
-import type { RouteOptions } from "@/app";
 import type { FastifyInstance } from "fastify";
 import type { IReply } from "@gazes/types";
+import type { RouteOptions } from "@/types/routeTypes";
 
 export default async function (app: FastifyInstance, opts: RouteOptions) {
-	app.get<{ Reply: IReply }>("/animes", (request, reply) => {});
+	app.get<{ Reply: IReply }>("/animes", (request, reply) => {
+
+	});
 }
