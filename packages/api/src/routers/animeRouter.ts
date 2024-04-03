ffac01fee7210500fbd74b1dbc79771079969fa7
Angelo Clauin <contact@l31.dev>
2024-04-03 13:19:12 +0200

    🚀 feat(api): setup database schema, routing, and services
    - 🚫 Add `.gitignore` to exclude `node_modules` and `.env` files from
      version control.
    - 🗃️ Introduce Prisma schema for PostgreSQL integration with an initial
      `Anime` model definition.
    - ✏️ Minor adjustments in `animeRouter.ts` to enhance readability with
      added whitespace.
    - 🏗️ Create `animeService.ts` for future business logic related to anime
      entities.

--- packages/api/src/routers/animeRouter.ts
+++ packages/api/src/routers/animeRouter.ts
@@ -3,7 +3,9 @@ import type { IReply } from "@gazes/types";
 import type { RouteOptions } from "@/types/routeTypes";
 
 export default async function (app: FastifyInstance, opts: RouteOptions) {
+
 	app.get<{ Reply: IReply }>("/animes", (request, reply) => {
 
 	});
+
 }
