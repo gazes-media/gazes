ed4625a2a8de5f1302d588fa2cc7761aab04f7cf
Angelo Clauin <contact@l31.dev>
2024-04-03 13:04:26 +0200

    ✨ feat(api): refactor routing and enhance code readability
    - 🎨 Improve code style in `app.ts` with better formatting and added comments for clarity.
    - 🔀 Rename `animes.router.ts` to `animeRouter.ts` and move to `routers` directory for consistent naming and organization.
    - 📝 Add `routeTypes.ts` for centralizing route-related type definitions, improving code maintainability.

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,17 +1,17 @@
-import { Glob } from "bun"
-import fastify from "fastify"
-import {join} from "node:path"
+import { Glob } from "bun";
+import fastify from "fastify";
+import { join } from "node:path";
 
+// Initialize a Fastify application instance with specific configurations.
 export const app = fastify({
-    logger: true,
-    disableRequestLogging: true,
-})
+	logger: true, // Enable loggin for the application
+	disableRequestLogging: true, // Disable request logging to avoid clutter in the log output
+});
 
-export type RouteOptions = {
-    prismaClient: unknown;
-    redisClient: unknown;
-}
-
-for await (const file of new Glob("**/*.router.*").scan(import.meta.dir)) {
-    app.register((await import(join(import.meta.dir, file))).default)
+// Synamically import and register router modules found by globbing thep roject directory for files
+// with a ".router." in their names.
+for await (const file of new Glob("**/*Router.*").scan(import.meta.dir)) {
+	// Register each found router module with the Fastify application.
+	// The router modules are expected to export a default function that defines routes.
+	app.register((await import(join(import.meta.dir, file))).default);
 }
---
+++ packages/api/src/types/routeTypes.ts
@@ -0,0 +1,7 @@
+// Route-related types and interfaces below
+
+// Define a type for route options, specifying the structure of objects passed to routes.
+export type RouteOptions = {
+    prismaClient: unknown;
+    redisClient: unknown;
+}
